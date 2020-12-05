890d09b7dc1f9e7cea9a71981fc37a21f9b8dea2
Hariom Vyas <u19ee014@eed.svnit.ac.in>
2020-12-05 11:58:43 +0530

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,7 @@
+import * as React from "react";
+import { render } from "react-dom";
+import createClass from 'create-react-class';
+
+const App = createClass({
+  render: () => <h1>This is coming from createClass() function</h1>,
+});
